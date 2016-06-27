3487e1fb0c2e3ab15e06418cbb0074976ebff672
cheatfate <ka@hardcore.kiev.ua>
2016-06-27 14:30:17 +0300

    Updated blank tests

--- tests/test_pool.nim
+++ tests/test_pool.nim
@@ -1,7 +1,5 @@
 import asyncdispatch, asyncpg
 
-proc testPool(conn: apgConnection) {.async.} =
-
 var connStr = "host=localhost port=5432 dbname=travis_ci_test user=postgres"
 var conn = waitFor connect(connStr)
 close(conn)
