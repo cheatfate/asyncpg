f33f6ef551b81c185193647fd15a2eab08f49c76
cheatfate <ka@hardcore.kiev.ua>
2016-06-28 09:09:13 +0300

    Tests updated

--- tests/test_utils.nim
+++ tests/test_utils.nim
@@ -3,18 +3,27 @@ import asyncdispatch, asyncpg
 proc testEncoding(conn: apgConnection): Future[bool] {.async.} =
   result = false
   var r = await setClientEncoding(conn, "WIN1252")
-  echo(r)
   if r:
     var s = getClientEncoding(conn)
-    echo(s)
     if s == "WIN1252":
       result = true
+
+proc testVersions(conn: apgConnection): bool =
+  result = false
+  if getVersion() > 0:
+    if getServerVersion(conn) > 0:
+      if getProtocolVersion(conn) > 0:
+        result = true
 
 var connStr = "host=localhost port=5432 dbname=travis_ci_test user=postgres"
 var conn = waitFor connect(connStr)
 
-block:
+block: # set/get encoding test
   var res = waitFor(testEncoding(conn))
+  doAssert(res)
+
+block: # protocol/server/api versions test
+  var res = testVersions(conn)
   doAssert(res)
 
 close(conn)
