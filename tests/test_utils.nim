dc61bba75dee0277a5f7f9bf490711940b57842b
cheatfate <ka@hardcore.kiev.ua>
2016-06-28 08:04:37 +0300

    Test modifications

--- tests/test_utils.nim
+++ tests/test_utils.nim
@@ -2,12 +2,12 @@ import asyncdispatch, asyncpg
 
 proc testEncoding(conn: apgConnection): Future[bool] {.async.} =
   result = false
-  var r = await setClientEncoding(conn, "WIN-1252")
+  var r = await setClientEncoding(conn, "WIN1252")
   echo(r)
   if r:
     var s = getClientEncoding(conn)
     echo(s)
-    if s == "WIN-1252":
+    if s == "WIN1252":
       result = true
 
 var connStr = "host=localhost port=5432 dbname=travis_ci_test user=postgres"
